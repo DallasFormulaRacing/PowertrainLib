e206006fa46a3d8bd41ced8527a3fc08ee22b1c6
Sahan Wick <syw210001@utdallas.edu>
2024-01-29 23:51:48 -0600

    Created initial wifi_client structure

---
+++ wifi_client/wifi_client.py
@@ -0,0 +1,20 @@
+import wifi
+from wifi import Cell
+
+class Client:
+    def __init__(self, device_id: str):
+        self.device_id = device_id
+
+    def connect(self, ssid: str, password: str) -> bool:
+        networks = Cell.all(self.device_id)
+        for network in networks:
+            if network.ssid == ssid:
+                scheme = wifi.Scheme.for_cell(self.device_id, network.ssid, network, password)
+                scheme.save()
+                scheme.activate()
+
+                print(f"Connected to wifi: {network.ssid}")
+                return True
+
+        print(f"Could not connect to wifi: {ssid}")
+        return False
