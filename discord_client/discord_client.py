0cf811b673277fde3dacb102e050acf589661cd7
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2024-02-13 11:17:12 -0600

    logging to discord client

--- discord_client/discord_client.py
+++ discord_client/discord_client.py
@@ -1,6 +1,9 @@
 import traceback
 
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 class Client:
@@ -8,16 +11,19 @@ class Client:
     def __init__(self, webhook_url: str):
         self.webhook_url = webhook_url
 
-    # make this return a boolean
     def post_message(self, message: str) -> bool:
         data = {
             "content": message
         }
-        result = requests.post(self.webhook_url, json=data)
 
         try:
+
+            logging.info("Posting message to Discord")
+            result = requests.post(self.webhook_url, json=data)
             result.raise_for_status()
+
         except requests.exceptions.HTTPError as err:
+            logging.exception("An error occurred: {}".format(err))
             traceback.print_exc(err)
         else:
             print("Status code: {}".format(result.status_code))
