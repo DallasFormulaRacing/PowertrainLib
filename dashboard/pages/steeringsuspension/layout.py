9cdb6744789747c91107fbdf12fbb88be975f3c2
unknown <oxs200002@utdallas.edu>
2024-04-12 16:34:49 -0500

    integrated wheel load vs time into powertrain lib

--- dashboard/pages/steeringsuspension/layout.py
+++ dashboard/pages/steeringsuspension/layout.py
@@ -1,8 +1,17 @@
 import dash
+from dash import html, dcc
 import dash_mantine_components as dmc
+from ..utils.analytics_page import make_components
+from .visualizations.wheel_load import wheel_load_over_time
+#from .visualizations.accel import accel_over_time
 
-dash.register_page(__name__, path="/suspension", name="Suspension", exact=True)
+dash.register_page(__name__, path="/suspensionsteering", name="SuspensionSteering")
 
-layout = dmc.Container([
-    dmc.Header("Suspension", height=0, className="text-4xl text-center")
-])
+graphs = {
+    "SuspensionSteering": {
+        "Wheel Load over Time": wheel_load_over_time
+        #"Acceleration vs Time": accel_over_time
+    }
+}
+
+layout = make_components(graphs)
---
+++ dashboard/pages/steeringsuspension/visualizations/wheel_load.py
@@ -0,0 +1,42 @@
+from dash import html, dcc
+import dash
+import dash_mantine_components as dmc
+from dash import callback
+from dash.dependencies import Input, Output
+import pandas as pd
+from ..SuspensionSteeringLib.client import client
+from pages.utils.graph_utils import color_seq
+import plotly.express as px
+import timeit
+
+PAGE = "SuspensionSteering"
+VIZ_ID = "wheel-load-over-time"
+
+wheel_load_over_time = dmc.Card(
+    [
+        dmc.Group(
+            [
+                html.H3(
+                    "Wheel Load vs Time (lbf)",
+                    className="card-title",
+                    style={"textAlign": "center"},
+                ),
+                dcc.Loading(
+                    dcc.Graph(id=f"{PAGE}-{VIZ_ID}"),
+                ),
+            ]
+        ),
+    ],
+)
+
+
+# callback for commits over time graph
+@callback(
+    Output(f"{PAGE}-{VIZ_ID}", "figure"),
+    Input("time-range", "data")
+)
+def wheel_load_over_accel_graph(_time_range):
+    clientInstance = client()
+    clientInstance.wheel_load_client()
+        
+    return clientInstance.wheel_load_client()
