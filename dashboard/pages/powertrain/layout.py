f42966911a7fc7ed53051bef93d9b8d9088a0d66
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2024-04-17 17:09:36 -0500

    Linpot graph demo

--- dashboard/pages/powertrain/layout.py
+++ dashboard/pages/powertrain/layout.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html, dcc
 import dash_mantine_components as dmc
-from ..utils.analytics_page import make_components
+from utils.analytics_page import make_components
 from .visualizations.rpm_over_time import gc_rpm_over_time
 from .visualizations.rpm_map_lambda import gc_rpm_map_lambda
 from .visualizations.tps_over_time import gc_tps_over_time
@@ -11,6 +11,7 @@ dash.register_page(__name__, path="/powertrain", name="Powertrain")
 
 graphs = {
     "Engine": {
+        "TPS Over Time": gc_tps_over_time,
         "RPM Over Time": gc_rpm_over_time,
         "RPM vs MAP vs Lambda/AFR LTF": gc_rpm_map_lambda,
         "Customizable ECU Data": gc_customizable_graph
--- dashboard/pages/powertrain/visualizations/tps_over_time.py
+++ dashboard/pages/powertrain/visualizations/tps_over_time.py
@@ -1,46 +1,74 @@
+from io import StringIO
 from dash import html, dcc
 import dash
 import dash_mantine_components as dmc
 from dash import callback
 from dash.dependencies import Input, Output
 import pandas as pd
-
-from pages.utils.graph_utils import color_seq
 import plotly.express as px
+from dash_iconify import DashIconify
 
 PAGE = "powertrain"
 VIZ_ID = "tps-over-time"
 
 gc_tps_over_time = dmc.Card(
-    [
-        dmc.Group(
+    id="rpm-over-time",
+    children=[
+        dmc.CardSection(
             [
-                html.H3(
-                    "TPS vs Time (sec)",
-                    className="card-title",
-                    style={"textAlign": "center"},
+                dmc.Group(
+                    children=[
+                        dmc.Text("TPS vs Time", weight=500),
+                        dmc.ActionIcon(
+                            DashIconify(icon="carbon:overflow-menu-horizontal"),
+                            color="gray",
+                            variant="transparent",
+                        ),
+                    ],
+                    position="apart",
                 ),
-                dcc.Loading(
-                    dcc.Graph(id=f"{PAGE}-{VIZ_ID}"),
+                dmc.Text(
+                    children=[
+                        "This graph shows the TPS of the engine over time. "
+                    ],
+                    mt="sm",
+                    color="dimmed",
+                    size="sm",
                 ),
-            ]
+            ],
+            inheritPadding=True,
+            py="xs",
+            withBorder=True,
+        ),
+        dmc.CardSection(
+            dcc.Loading(
+                dcc.Graph(id=f"{PAGE}-{VIZ_ID}"),
+            ),
         ),
     ],
+    withBorder=True,
+    shadow="sm",
+    radius="md",
+    p="xs",
+    m="xs",
+    bg="black",
+    style={"width": "100%"},
 )
 
 
 # callback for commits over time graph
 @callback(
     Output(f"{PAGE}-{VIZ_ID}", "figure"),
-    Input("time-range", "data")
+    Input("ljm-data", "data")
 )
-def tps_over_time_graph(_time_range):
-    df = pd.read_csv('./ecu_data.csv', header="infer")
-
+def tps_over_time_graph(data):
+    df = pd.read_json(data, orient='split', encoding='unicode_escape')
+    print("read df: ", df)
+    if df.empty:
+        return px.line(title="No Data", labels={"value": "TPS", "timestamp": "Time"})
     fig = px.line(
         df,
-        x="Time (sec)",
-        y="TPS (%)",
-        labels={"value": "TPS", "timestamp": "Time"}
+        x="timestamp",
+        y=["Front Left", "Front Right", "Rear Left", "Rear Right"],
     )
     return fig
