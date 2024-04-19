6c4355745cec4df3c04d0a64efd97faafa73cad6
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2024-04-19 04:27:47 -0500

    rename relative imports

--- dashboard/pages/steeringsuspension/layout.py
+++ dashboard/pages/steeringsuspension/layout.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html, dcc
 import dash_mantine_components as dmc
-from ..utils.analytics_page import make_components
+from utils.analytics_page import make_components
 from .visualizations.wheel_load import wheel_load_over_time
 
 dash.register_page(__name__, path="/suspensionsteering", name="SuspensionSteering")
