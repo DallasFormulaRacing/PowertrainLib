9ccfdf0f973f5b54c9ca421746bdaedf8000387e
unknown <oxs200002@utdallas.edu>
2024-04-23 13:31:13 -0500

    Ofek Integration Push

--- dashboard/pages/steeringsuspension/layout.py
+++ dashboard/pages/steeringsuspension/layout.py
@@ -1,8 +1,19 @@
 import dash
+from dash import html, dcc
 import dash_mantine_components as dmc
+from ..utils.analytics_page import make_components
+from .visualizations.wheel_load import wheel_load_over_time
+from .visualizations.pitch_roll import pitch_roll_over_time
+from .visualizations.linpot import linpot_over_time
 
-dash.register_page(__name__, path="/suspension", name="Suspension", exact=True)
+dash.register_page(__name__, path="/suspensionsteering", name="SuspensionSteering")
 
-layout = dmc.Container([
-    dmc.Header("Suspension", height=0, className="text-4xl text-center")
-])
+graphs = {
+    "SuspensionSteering": {
+        "Wheel Load over Time": wheel_load_over_time,
+        "Linpot over Time": linpot_over_time,
+        "Pitch and Roll over Time": pitch_roll_over_time
+    }
+}
+
+layout = make_components(graphs)
---
+++ dashboard/pages/steeringsuspension/visualizations/pitch_roll.py
@@ -0,0 +1,40 @@
+from dash import html, dcc
+import dash
+import dash_mantine_components as dmc
+from dash import callback
+from dash.dependencies import Input, Output
+import pandas as pd
+from ..SuspensionSteeringLib.client import client
+from pages.utils.graph_utils import color_seq
+import plotly.express as px
+import timeit
+
+PAGE = "SuspensionSteering"
+VIZ_ID = "pitch-roll-over-time"
+
+pitch_roll_over_time = dmc.Card(
+    [
+        dmc.Group(
+            [
+                html.H3(
+                    "Pitch and Roll vs Time (deg)",
+                    className="card-title",
+                    style={"textAlign": "center"},
+                ),
+                dcc.Loading(
+                    dcc.Graph(id=f"{PAGE}-{VIZ_ID}"),
+                ),
+            ]
+        ),
+    ],
+)
+
+
+# callback for commits over time graph
+@callback(
+    Output(f"{PAGE}-{VIZ_ID}", "figure"),
+    Input("time-range", "data")
+)
+def pitch_roll_over_time_graph(_time_range):
+    clientInstance = client()
+    return clientInstance.pitch_roll_client()
