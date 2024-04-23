0ac8b1983ee2846219d8cd9c71c972f1c1ff6523
Ani2nem <ani2nem@gmail.com>
2024-04-23 14:04:26 -0500

    added damper_force.py and damper_velocity files to visualizations, updated layout.py to reflect that

--- dashboard/pages/steeringsuspension/layout.py
+++ dashboard/pages/steeringsuspension/layout.py
@@ -5,6 +5,8 @@ from ..utils.analytics_page import make_components
 from .visualizations.wheel_load import wheel_load_over_time
 from .visualizations.pitch_roll import pitch_roll_over_time
 from .visualizations.linpot import linpot_over_time
+from .visualizations.damper_force import damper_force_over_time
+from .visualizations.damper_velocity import damper_velocity_over_time
 
 dash.register_page(__name__, path="/suspensionsteering", name="SuspensionSteering")
 
@@ -12,7 +14,9 @@ graphs = {
     "SuspensionSteering": {
         "Wheel Load over Time": wheel_load_over_time,
         "Linpot over Time": linpot_over_time,
-        "Pitch and Roll over Time": pitch_roll_over_time
+        "Pitch and Roll over Time": pitch_roll_over_time,
+        "Damper Force over Time": damper_force_over_time,
+        "Damper Velocity over Time": damper_velocity_over_time
     }
 }
 
---
+++ dashboard/pages/steeringsuspension/visualizations/damper_velocity.py
@@ -0,0 +1,42 @@
+from dash import html, dcc
+import dash
+import dash_mantine_components as dmc
+from dash import callback
+from dash.dependencies import Input, Output
+import pandas as pd
+from ..SuspensionSteeringLib.client import client
+from pages.utils.graph_utils import color_seq
+import plotly.express as px
+import timeit
+
+PAGE = "SuspensionSteering"
+VIZ_ID = "damper-velocity-vs-time"
+
+damper_velocity_over_time = dmc.Card(
+    [
+        dmc.Group(
+            [
+                html.H3(
+                    "Damper Velocity vs Time",
+                    className="card-title",
+                    style={"textAlign": "center"},
+                ),
+                dcc.Loading(
+                    dcc.Graph(id=f"{PAGE}-{VIZ_ID}"),
+                ),
+            ]
+        ),
+    ],
+)
+
+
+# callback for commits over time graph
+@callback(
+    Output(f"{PAGE}-{VIZ_ID}", "figure"),
+    Input("time-range", "data")
+)
+def damper_velocity_over_time_graph(_time_range):
+    clientInstance = client()
+    clientInstance.damper_velocity_vs_time_client()
+        
+    return clientInstance.damper_velocity_vs_time_client()
